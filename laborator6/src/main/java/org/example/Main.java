7c765f4ebd376bd75bfcc5b02d94591f73add118
MagdaTautu <51873232+MagdaTautu@users.noreply.github.com>
2023-04-08 19:49:32 +0300

    Add files via upload

---
+++ laborator6/src/main/java/org/example/Main.java
@@ -0,0 +1,21 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setVisible(true);
+//        frame.setTitle("My drawing application");
+//        frame.setLayout(new FlowLayout(FlowLayout.LEFT));
+//        frame.setSize(800,600);
+//        ConfigPanel configPanel = new ConfigPanel(frame);
+//        DrawingPanel drawingPanel = new DrawingPanel(frame);
+//        frame.add(configPanel);
+//        frame.add(drawingPanel);
+//        frame.setVisible(true);
+
+
+    }
+}
